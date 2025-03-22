535a619756dbc27570ee3ecc7f1e1a4dc471f515
Pedro Ribas <109811039+oribaspedro@users.noreply.github.com>
2025-03-21 22:45:07 -0300

    Delete ExemploAviao directory

--- ExemploAviao/src/ExemploAviao.java
+++
@@ -1,9 +0,0 @@
-public class ExemploAviao {
-    public static void main(String[] args) {
-        Aviao aviao1 = new Aviao();
-        aviao1.fabricante = "Boeing";
-        aviao1.modelo = "727";
-        aviao1.qtdAssentos = 321;
-        aviao1.ligar();
-    }
-}
